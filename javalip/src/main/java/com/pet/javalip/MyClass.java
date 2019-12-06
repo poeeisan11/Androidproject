c5a9e04f32fa76ec6f01f4c68c71b853098530e3
poeeisan11 <phyoeithwe0901@gmail.com>
2019-12-06 10:28:04 +0630

    first commit

---
+++ javalip/src/main/java/com/pet/javalip/MyClass.java
@@ -0,0 +1,23 @@
+package com.pet.javalip;
+import java.lang.*;
+public class MyClass {
+    public static void main(String args[])
+    {
+        int a=5;
+        int m,n;
+        int s=5;
+        int b=2;
+        int c=1;
+        int x=10;
+        int y=2;
+        int p=2;
+        m=++a*5;
+        p*=x/y;
+        s/=5;
+        n=b++ - c*2;
+        System.out.println(m);
+        System.out.println(p);
+        System.out.println(s);
+        System.out.println(n);
+    }
+}
